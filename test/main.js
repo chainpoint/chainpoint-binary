7ee3a872befca6b71a9ae7b8a8deda5d4e6df6f5
Glenn Rempe <glenn@tierion.com>
2017-04-30 17:22:39 -0700

    Use consistent terminology in tests as in the README

--- test/main.js
+++ test/main.js
@@ -4,21 +4,21 @@ const should = require('should')
 const cb = require('../index')
 const fs = require('fs')
 
-describe('Testing valid format receipts should convert to CHP and back to same source JSON ', function () {
-  describe('Using test1.json - ', function () {
-    it('JSON >> CHP >> JSON should return result equal to origin JSON', function (done) {
-      fs.readFile('./test/json/test1.json', 'utf-8', function (err, sourceFileJSON) {
+describe('A valid proof converted to binary and back ', function () {
+  describe('Using test1.json', function () {
+    it('should return proof equal to original JSON', function (done) {
+      fs.readFile('./test/json/test1.json', 'utf-8', function (err, jsonSample) {
         should.not.exist(err)
-        should.exist(sourceFileJSON)
-        cb.objectToBinary(sourceFileJSON, function (err, chpData) {
+        should.exist(jsonSample)
+        cb.objectToBinary(jsonSample, function (err, proofBinary) {
           should.not.exist(err)
-          should.exist(chpData)
-          cb.binaryToObject(chpData, function (err, resultObject) {
+          should.exist(proofBinary)
+          cb.binaryToObject(proofBinary, function (err, proofObject) {
             should.not.exist(err)
-            should.exist(resultObject)
-            sourceFileJSON = sourceFileJSON.replace(/(\r\n|\n|\r| )/gm, '') // remove any whitespace/cf/lf
-            let resultJSON = JSON.stringify(resultObject)
-            resultJSON.should.equal(sourceFileJSON)
+            should.exist(proofObject)
+            jsonSample = jsonSample.replace(/(\r\n|\n|\r| )/gm, '') // remove any whitespace/cf/lf
+            let resultJSON = JSON.stringify(proofObject)
+            resultJSON.should.equal(jsonSample)
             done()
           })
         })
